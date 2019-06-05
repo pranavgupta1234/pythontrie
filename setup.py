bac1c940eb35349ebafc53c3aed2824be2d383ac
pranavgupta1234 <pranavgupta4321@gmail.com>
2019-06-05 19:49:52 +0530

    setup.py updated with repo link and .travis.yml with before_install

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='ptrie',
-     version='0.1',
-     scripts=['ptrie'] ,
-     author="Pranav Gupta",
-     author_email="pranavgupta4321@gmail.com",
-     description="General Purpose Trie Data Structure in Python",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/javatechy/dokr",
-     packages=setuptools.find_packages(),
-     classifiers=[
+    name='ptrie',
+    version='0.1',
+    scripts=['ptrie'] ,
+    py_modules=["ptrie"],
+    author="Pranav Gupta",
+    author_email="pranavgupta4321@gmail.com",
+    description="General Purpose Trie Data Structure in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pranavgupta1234/ptrie",
+    packages=setuptools.find_packages(),
+    classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
 
@@ -22,5 +23,5 @@ setuptools.setup(
 
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
-     ],
+    ],
  )
