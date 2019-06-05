3d477f191759ae0e8585acca4fe2997ee792f546
pranavgupta1234 <pranavgupta4321@gmail.com>
2019-06-06 00:58:46 +0530

    bumping to v0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pythontrie',
-    version='0.1',
+    version='0.2',
     author="Pranav Gupta",
     author_email="pranavgupta4321@gmail.com",
     description="General Purpose Trie Data Structure in Python",
