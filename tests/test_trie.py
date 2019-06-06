cf05e85bb1e59e271da262085ddcf0ca1e7c49cb
pranavgupta1234 <pranavgupta4321@gmail.com>
2019-06-06 10:29:54 +0530

    added more tests for trie suggestions

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -13,3 +13,18 @@ class TrieBasicTests(unittest.TestCase):
         for sample_string in sample_strings:
             sample_trie.insert(sample_string)
         self.assertEqual(sample_trie.size(), 7)
+
+    def test_trie_size_of_suggestions(self):
+        sample_trie = trie()
+        sample_strings = ['heyy', 'heyay', 'heyey', 'hyyy', 'yoyo', 'heeyy', 'hoeyy']
+        for sample_string in sample_strings:
+            sample_trie.insert(sample_string)
+        suggested = sample_trie.suggestions(prefix='he')
+        self.assertEqual(len(suggested), 4)
+
+    def test_trie_search(self):
+        sample_trie = trie()
+        sample_strings = ['heyy', 'heyay',]
+        for sample_string in sample_strings:
+            sample_trie.insert(sample_string)
+        self.assertEqual(sample_trie.search("this"), False)
